a39b89ee1ab5daf6ee57f8a9f4b08264a97429a1
alapalapa <roberto-750@hotmail.com>
2014-07-02 18:49:18 -0500

    project updated with keys and use of methods

---
+++ getTweets.php
@@ -0,0 +1,45 @@
+<?php
+
+//require the twitteroauth (API Twitter)
+require_once("oauth/twitteroauth.php"); 
+
+
+//Save the information sended by POST from angularjs
+$post_data 	= file_get_contents("php://input");
+$request 	= json_decode($post_data);
+$usuario 	= $request->user;
+$password 	= $request->pass;
+
+
+//Specify the name user and number of tweets to obtain
+$name_user 		= "FutApp";
+$number_tweets 	= 5;
+
+
+//Privated data for authentification
+$consumer_key 			= "tZfK0PjSpkwdoYLq3qSHjlXGy";
+$consumer_secret 		= "MXXsIxgdQ0ulFrqlyR0f6aROM4wPmEmcOe1W3Z4FFpXhESzFgo";
+$access_token 			= "2542735675-AnseFjpZGpgEmFbS0IcQJx37faiWPFVhTN6yqCi";
+$access_token_secret 	= "raIcBt69nplFyiR82WU4c8NoGgJRxNtRMmcLRAwjK1gyy";
+
+
+//Declaration of the function to validate access token
+function connectWithToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+
+
+//Validation of the information with access token
+$connection = connectWithToken($consumer_key, $consumer_secret, $access_token, $access_token_secret);
+
+
+//Call to a function to get tweets with valid information
+$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$name_user."&count=".$number_tweets);
+
+
+//Encoding data for the response
+echo json_encode($tweets);
+
+
+?>
--- js/getTweets.js
+++ js/getTweets.js
@@ -1,6 +1,6 @@
 (function(){
-    var app = angular.module('request', []);
-    
+    var app = angular.module('twitterApi', []);
+
 
     app.controller('TwitterController', ['$http', function($http){
         this.tweets = [];
@@ -10,22 +10,15 @@
 
             var request = $http({
                 method: "POST",
-                url:"get-tweets.php",
+                url:"getTweets.php",
+                headers: {'Authorization': 'xxx', 'Content-Type': 'application/json'},
                 data:{
                     'user':"user1234",
                     'pass':"pass1234"
                 }
             })            
             .success(function(data){
-                console.log('success');
                 twitterCtrl.tweets = data;
-                console.log(twitterCtrl.tweets);
-                /*
-                console.log(this.tweets[1].text);
-                console.log(this.tweets[1].created_at);
-                console.log(this.tweets[1].user.name);
-                console.log(this.tweets[1].source);
-                */
             }).error(function(){
                 console.log('error');
             });
@@ -34,3 +27,4 @@
     }]);
 
 })();
+
